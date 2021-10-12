dccea167bda8cb67ad642fee2e10bde38d96b1a7
gabriellenoel <gabby@gabriellealexa.com>
2021-10-12 16:16:21 -0400

    finished testing trip card

--- src/components/flight-list/TripCard.test.js
+++ src/components/flight-list/TripCard.test.js
@@ -86,3 +86,169 @@ xit("calls #onTripSelection when Economy Class is clicked", async () => {
         SeatClass.ECONOMY
     );
 });
+
+xit("calls #onTripSelection when Business Class is clicked", async () => {
+    const handleTripSelection = jest.fn();
+    const trip = [{
+        id: 1,
+        airplane: {
+            id: 16,
+            firstClassSeatsMax: 20,
+            businessClassSeatsMax: 50,
+            economyClassSeatsMax: 280,
+            model: "Airbus A220"
+        },
+        departureTime: "2021-08-07T16:15:00.000+00:00",
+        arrivalTime: "2021-08-07T17:33:00.000+00:00",
+        firstReserved: 0,
+        firstPrice: 100.00,
+        businessReserved: 0,
+        businessPrice: 100.00,
+        economyReserved: 0,
+        economyPrice: 100.00,
+        isActive: true,
+        route: {
+            id:21,
+            originAirport: {
+                iataId: "JFK",
+                city: "New York City",
+                isActive:true
+            },
+            destinationAirport: {
+                iataId: "SFO",
+                city: "San Francisco",
+                isActive: true
+            },
+                isActive: true
+            }
+    },
+    {
+        id: 2,
+        airplane: {
+            id: 17,
+            firstClassSeatsMax: 20,
+            businessClassSeatsMax: 50,
+            economyClassSeatsMax: 280,
+            model: "Airbus A220"
+        },
+        departureTime: "2021-08-07T16:15:00.000+00:00",
+        arrivalTime: "2021-08-07T17:33:00.000+00:00",
+        firstReserved: 0,
+        firstPrice: 100.00,
+        businessReserved: 0,
+        businessPrice: 100.00,
+        economyReserved: 0,
+        economyPrice: 100.00,
+        isActive: true,
+        route: {
+            id:22,
+            originAirport: {
+                iataId: "SFO",
+                city: "San Francisco",
+                isActive:true
+            },
+            destinationAirport: {
+                iataId: "LAX",
+                city: "Los Angeles",
+                isActive: true
+            },
+                isActive: true
+            }
+    }
+]
+
+    render(
+        <TripCard trip={trip} onTripSelection={handleTripSelection} />
+    );
+
+    const businessPrice = SeatClass.BUSINESS;
+    userEvent.click(screen.getByText("$90"));
+
+    expect(handleTripSelection).toHaveBeenCalledWith(
+        trip,
+        SeatClass.BUSINESS
+    );
+});
+
+xit("calls #onTripSelection when First Class is clicked", async () => {
+    const handleTripSelection = jest.fn();
+    const trip = [{
+        id: 1,
+        airplane: {
+            id: 16,
+            firstClassSeatsMax: 20,
+            businessClassSeatsMax: 50,
+            economyClassSeatsMax: 280,
+            model: "Airbus A220"
+        },
+        departureTime: "2021-08-07T16:15:00.000+00:00",
+        arrivalTime: "2021-08-07T17:33:00.000+00:00",
+        firstReserved: 0,
+        firstPrice: 100.00,
+        businessReserved: 0,
+        businessPrice: 100.00,
+        economyReserved: 0,
+        economyPrice: 100.00,
+        isActive: true,
+        route: {
+            id:21,
+            originAirport: {
+                iataId: "JFK",
+                city: "New York City",
+                isActive:true
+            },
+            destinationAirport: {
+                iataId: "SFO",
+                city: "San Francisco",
+                isActive: true
+            },
+                isActive: true
+            }
+    },
+    {
+        id: 2,
+        airplane: {
+            id: 17,
+            firstClassSeatsMax: 20,
+            businessClassSeatsMax: 50,
+            economyClassSeatsMax: 280,
+            model: "Airbus A220"
+        },
+        departureTime: "2021-08-07T16:15:00.000+00:00",
+        arrivalTime: "2021-08-07T17:33:00.000+00:00",
+        firstReserved: 0,
+        firstPrice: 100.00,
+        businessReserved: 0,
+        businessPrice: 100.00,
+        economyReserved: 0,
+        economyPrice: 100.00,
+        isActive: true,
+        route: {
+            id:22,
+            originAirport: {
+                iataId: "SFO",
+                city: "San Francisco",
+                isActive:true
+            },
+            destinationAirport: {
+                iataId: "LAX",
+                city: "Los Angeles",
+                isActive: true
+            },
+                isActive: true
+            }
+    }
+]
+
+    render(
+        <TripCard trip={trip} onTripSelection={handleTripSelection} />
+    );
+
+    const firstPrice = SeatClass.FIRST;
+    userEvent.click(screen.getByText("$90"));
+
+    expect(handleTripSelection).toHaveBeenCalledWith(
+        trip,
+        SeatClass.FIRST
+    );
+});
